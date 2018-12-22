b4b56ede590bf54b78479d0ca18398cb86cc42e3
robotmayo <robotmayo@gmail.com>
2018-12-21 21:49:02 -0500

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,242 @@
+import { Context } from "koa";
+import * as Busboy from "busboy";
+import { EventEmitter } from "events";
+import { randomBytes } from "crypto";
+import { strict as assert } from "assert";
+import * as fs from "fs";
+import { join } from "path";
+import { Writable } from "stream";
+const appendField = require("append-field");
+
+declare module "koa" {
+  interface Request {
+    files: KMultipartFile[];
+    body?: { [key: string]: any };
+  }
+}
+
+export interface KMultipartFile {
+  originalFilename: string;
+  fieldname: string;
+}
+
+export interface MemoryStorageFile extends KMultipartFile {
+  fileContents: Buffer;
+}
+
+export interface HandleFileObj {
+  file: NodeJS.ReadableStream;
+  filename: string;
+  fieldname: string;
+  ctx: Context;
+  encoding: string;
+  mimetype: string;
+}
+
+interface StorageEngine {
+  handleFile: (h: HandleFileObj) => Promise<any>;
+}
+
+async function genFileName(h: HandleFileObj) {
+  return randomBytes(8).toString("hex");
+}
+
+export class ConcatStream extends Writable {
+  body: Buffer[] = [];
+  constructor() {
+    super();
+  }
+  _write(chunk: Buffer, encoding?: string, cb?: (err?: Error) => void) {
+    this.body.push(chunk);
+    cb();
+  }
+  bodyBuffer() {
+    return Buffer.concat(this.body);
+  }
+}
+
+export class MemoryStorage implements StorageEngine {
+  async handleFile(h: HandleFileObj) {
+    return new Promise((resolve, reject) => {
+      const c = new ConcatStream();
+      h.file
+        .pipe(c)
+        .once("finish", () => {
+          resolve({ fileContents: c.bodyBuffer() });
+        })
+        .once("error", reject);
+    });
+  }
+}
+
+export interface DiskStorageFile extends KMultipartFile {
+  size: number;
+  path: string;
+  filename: string;
+}
+
+export class DiskStorage implements StorageEngine {
+  genFileName = genFileName;
+  genDestination?: (h: HandleFileObj) => Promise<string>;
+  destination?: string;
+  filenameAndDestinationHandler: (
+    h: HandleFileObj
+  ) => Promise<{ destination: string; filename: string }>;
+
+  constructor(opts: {
+    filenameGenerator?: (h: HandleFileObj) => Promise<string>;
+    destinationGenerator?: (h: HandleFileObj) => Promise<string>;
+    destination?: string;
+  }) {
+    assert(
+      (!opts.destination && !opts.destinationGenerator) === false,
+      "must specify a destination or destination generator function"
+    );
+
+    if (opts.destination) {
+      assert(
+        typeof opts.destination === "string",
+        "destination must be a string"
+      );
+    }
+
+    if (opts.destinationGenerator) {
+      assert(
+        typeof opts.destinationGenerator === "function",
+        "destinationGenerator must be a function"
+      );
+    }
+
+    if (opts.filenameGenerator) {
+      assert(
+        typeof opts.filenameGenerator === "function",
+        "filenameGenerator must be a function"
+      );
+    }
+    this.destination = opts.destination;
+  }
+  async handleFile(h: HandleFileObj) {
+    const filename = await this.genFileName(h);
+    const destination = this.destination || (await this.genDestination(h));
+    const fullPath = join(destination, filename);
+    const fileStream = fs.createWriteStream(fullPath);
+    h.file.pipe(fileStream);
+    return new Promise((resolve, reject) => {
+      fileStream.on("error", reject).on("finish", () => {
+        resolve({
+          size: fileStream.bytesWritten,
+          path: fullPath,
+          filename
+        });
+      });
+    });
+  }
+}
+
+interface MultiPartOptions {
+  maxFileSize?: number;
+  maxNumFiles?: number;
+  storageEngine: StorageEngine;
+}
+
+const defaultOpts = {
+  // Max file size. Default 200mb
+  maxFileSize: 200 * 1024 * 1024,
+  maxNumFiles: 20
+};
+
+class Counter extends EventEmitter {
+  val: number = 0;
+  increment() {
+    this.val++;
+  }
+  decrement() {
+    this.val--;
+    if (this.val === 0) this.emit("zero");
+  }
+  whenZero(cb: () => void) {
+    if (this.val === 0) return cb();
+    this.once("zero", cb);
+  }
+}
+
+export function multipart(multipartOpts: MultiPartOptions) {
+  const opts = Object.assign({}, defaultOpts, multipartOpts);
+  assert(opts.storageEngine != null, "a storage engine must be provided");
+
+  return async function multipartMiddleware(ctx: Context, next: any) {
+    if (!ctx.is("multipart")) return next();
+    ctx.request.files = [];
+    const fields: { [key: string]: any } = {};
+    const busboy = new Busboy({
+      headers: ctx.req.headers,
+      limits: {
+        fileSize: opts.maxFileSize,
+        files: opts.maxNumFiles
+      }
+    });
+    let pending = new Counter();
+    await new Promise((resolve, reject) => {
+      ctx.req.pipe(busboy);
+      let isDone = false;
+      let busboyIsFinished = false;
+      let errored = false;
+      function checkDone() {
+        if (
+          busboyIsFinished === true &&
+          pending.val === 0 &&
+          errored === false
+        ) {
+          done();
+        }
+      }
+      function errorOut(err: Error) {
+        if (errored) return;
+        errored = true;
+        pending.whenZero(() => {
+          reject(err);
+        });
+      }
+      function done(err?: Error) {
+        if (isDone) return;
+        isDone = true;
+        ctx.req.unpipe(busboy);
+        busboy.removeAllListeners();
+        ctx.request.body = Object.assign(ctx.request.body || {}, fields);
+        pending.whenZero(() => {
+          resolve();
+        });
+      }
+
+      busboy
+        .on("field", (name, value) => {
+          appendField(fields, name, value);
+        })
+        .on("file", (fieldname, file, filename, encoding, mimetype) => {
+          // if we errored dont call handlefile for remaining files
+          if (errored) return file.resume();
+          pending.increment();
+          opts.storageEngine
+            .handleFile({ file, filename, fieldname, ctx, encoding, mimetype })
+            .then(res => {
+              ctx.request.files.push(
+                Object.assign({ originalFilename: filename, fieldname }, res)
+              );
+              pending.decrement();
+
+              checkDone();
+            })
+            .catch(err => {
+              pending.decrement();
+              errorOut(err);
+            });
+        })
+        .on("finish", () => {
+          busboyIsFinished = true;
+          checkDone();
+        })
+        .on("error", errorOut);
+    });
+    return next();
+  };
+}
---
+++ src/kmultipart.test.ts
@@ -0,0 +1,278 @@
+import anyTest, { TestInterface } from "ava";
+import * as http from "http";
+import {
+  ConcatStream,
+  multipart,
+  MemoryStorage,
+  KMultipartFile,
+  MemoryStorageFile,
+  DiskStorage,
+  DiskStorageFile
+} from ".";
+import { createReadStream, readFileSync, promises, statSync } from "fs";
+import * as Koa from "koa";
+import { resolve, basename } from "path";
+import fetch from "node-fetch";
+import * as FormData from "form-data";
+import { tmpdir } from "os";
+const FIXTURE_DIR = resolve(__dirname, "..", "fixtures");
+const FIXTURE_PATHS = {
+  SHORT_TEXT: resolve(FIXTURE_DIR, "short.txt"),
+  BIG_JPEG: resolve(FIXTURE_DIR, "big.jpeg"),
+  SMALL_JPEG: resolve(FIXTURE_DIR, "small.jpeg"),
+  EMPTY: resolve(FIXTURE_DIR, "empty"),
+  LONG_TEXT: resolve(FIXTURE_DIR, "long.txt")
+};
+const test = anyTest as TestInterface<{
+  listeners: http.Server[];
+  listen: (
+    app: Koa
+  ) => Promise<{
+    server: http.Server;
+    address: string;
+    port: number;
+    family: string;
+  }>;
+}>;
+test.before(async t => {
+  const listeners: http.Server[] = [];
+  t.context.listeners = listeners;
+  t.context.listen = async app => {
+    return new Promise((resolve, reject) => {
+      const s = app.listen(0);
+      listeners.push(s);
+      s.once("listening", () => {
+        s.off("error", reject);
+        resolve({ server: s, ...(s.address as any)() });
+      }).once("error", reject);
+    });
+  };
+});
+test.after(t => {
+  for (const server of t.context.listeners) {
+    server.close();
+  }
+});
+
+test.cb("concat stream > simple text file", t => {
+  t.plan(1);
+  const cs = new ConcatStream();
+  const rs = createReadStream(FIXTURE_PATHS.SHORT_TEXT);
+  rs.pipe(cs)
+    .on("finish", () => {
+      const text = cs.bodyBuffer().toString("utf8");
+      const file = readFileSync(FIXTURE_PATHS.SHORT_TEXT, "utf8");
+      t.is(text, file);
+      t.end();
+    })
+    .on("error", err => t.fail(err.stack));
+});
+
+test.cb("concat stream > big jpeg", t => {
+  t.plan(1);
+  const cs = new ConcatStream();
+  const rs = createReadStream(FIXTURE_PATHS.SMALL_JPEG);
+  rs.pipe(cs)
+    .on("finish", () => {
+      const imgBuffer = cs.bodyBuffer();
+      const file = readFileSync(FIXTURE_PATHS.SMALL_JPEG);
+      t.is(imgBuffer.length, file.length);
+      t.end();
+    })
+    .on("error", err => t.fail(err.message));
+});
+
+test("multipart > memory storage", async t => {
+  const app = new Koa();
+  t.plan(5);
+  app.use(multipart({ storageEngine: new MemoryStorage() })).use(ctx => {
+    ctx.body = "im done";
+    const files = ctx.request.files as MemoryStorageFile[];
+    t.is(files.length, 1);
+    t.is(files[0].originalFilename, basename(FIXTURE_PATHS.SMALL_JPEG));
+    t.is(files[0].fieldname, "SMALLER");
+    t.is(
+      files[0].fileContents.length,
+      readFileSync(FIXTURE_PATHS.SMALL_JPEG).length
+    );
+    t.true(Buffer.isBuffer(files[0].fileContents));
+  });
+  const { port } = await t.context.listen(app);
+  const form = new FormData();
+  form.append("SMALLER", createReadStream(FIXTURE_PATHS.SMALL_JPEG));
+  await fetch("http://localhost:" + port, {
+    method: "post",
+    headers: form.getHeaders(),
+    body: form
+  });
+});
+
+test("multipart > memory storage > empty file", async t => {
+  const app = new Koa();
+  t.plan(6);
+  app.use(multipart({ storageEngine: new MemoryStorage() })).use(ctx => {
+    ctx.body = "im done";
+    const body = ctx.request.body;
+    const files = ctx.request.files as (KMultipartFile & MemoryStorageFile)[];
+    t.is(files.length, 1);
+    t.is(files[0].fileContents.length, 0);
+    t.is(files[0].originalFilename, "empty");
+    t.is(files[0].fieldname, "nothing here");
+    t.is(body.name, "sanic");
+    t.is(body["this is also empty"], "");
+  });
+  const { port } = await t.context.listen(app);
+  const form = new FormData();
+  form.append("nothing here", createReadStream(FIXTURE_PATHS.EMPTY));
+  form.append("this is also empty", "");
+  form.append("name", "sanic");
+  await fetch("http://localhost:" + port, {
+    method: "post",
+    headers: form.getHeaders(),
+    body: form
+  });
+});
+
+test("multipart > memory storage > multiple files", async t => {
+  const app = new Koa();
+  t.plan(8);
+  app.use(multipart({ storageEngine: new MemoryStorage() })).use(ctx => {
+    ctx.body = "im done";
+    const body = ctx.request.body;
+    const files = ctx.request.files as MemoryStorageFile[];
+    t.is(files.length, 3);
+    for (const file of files) {
+      switch (file.originalFilename) {
+        case basename(FIXTURE_PATHS.SMALL_JPEG):
+          t.is(file.fieldname, "animage");
+          t.is(
+            file.fileContents.length,
+            readFileSync(FIXTURE_PATHS.SMALL_JPEG).length
+          );
+          break;
+        case basename(FIXTURE_PATHS.SHORT_TEXT):
+          t.is(file.fieldname, "sometext");
+          t.is(
+            file.fileContents.length,
+            readFileSync(FIXTURE_PATHS.SHORT_TEXT).length
+          );
+          break;
+        case basename(FIXTURE_PATHS.LONG_TEXT):
+          t.is(file.fieldname, "alottext");
+          t.is(
+            file.fileContents.length,
+            readFileSync(FIXTURE_PATHS.LONG_TEXT).length
+          );
+          break;
+        default:
+          t.fail("Unknown file found");
+      }
+    }
+    t.is(body.name, "sanic");
+  });
+  const { port } = await t.context.listen(app);
+  const form = new FormData();
+  form.append("animage", createReadStream(FIXTURE_PATHS.SMALL_JPEG));
+  form.append("sometext", createReadStream(FIXTURE_PATHS.SHORT_TEXT));
+  form.append("alottext", createReadStream(FIXTURE_PATHS.LONG_TEXT));
+  form.append("name", "sanic");
+  await fetch("http://localhost:" + port, {
+    method: "post",
+    headers: form.getHeaders(),
+    body: form
+  });
+});
+
+test("multipart > disk storage", async t => {
+  const app = new Koa();
+  t.plan(2);
+  const storage = new DiskStorage({ destination: tmpdir() });
+  app.use(multipart({ storageEngine: storage })).use(ctx => {
+    ctx.body = "im done";
+    const files = ctx.request.files as DiskStorageFile[];
+    t.truthy(statSync(files[0].path));
+    t.is(files[0].size, readFileSync(FIXTURE_PATHS.SMALL_JPEG).byteLength);
+  });
+  const { port } = await t.context.listen(app);
+  const form = new FormData();
+  form.append("SMALLER", createReadStream(FIXTURE_PATHS.SMALL_JPEG));
+  await fetch("http://localhost:" + port, {
+    method: "post",
+    headers: form.getHeaders(),
+    body: form
+  });
+});
+
+test("multipart > disk storage > multiple files", async t => {
+  const app = new Koa();
+  t.plan(7);
+  const storage = new DiskStorage({ destination: tmpdir() });
+  app.use(multipart({ storageEngine: storage })).use(ctx => {
+    ctx.body = "im done";
+    const files = ctx.request.files as DiskStorageFile[];
+    for (const file of files) {
+      switch (file.originalFilename) {
+        case basename(FIXTURE_PATHS.SMALL_JPEG):
+          t.is(file.fieldname, "animage");
+          t.is(file.size, readFileSync(FIXTURE_PATHS.SMALL_JPEG).byteLength);
+          break;
+        case basename(FIXTURE_PATHS.SHORT_TEXT):
+          t.is(file.fieldname, "sometext");
+          t.is(file.size, readFileSync(FIXTURE_PATHS.SHORT_TEXT).byteLength);
+          break;
+        case basename(FIXTURE_PATHS.LONG_TEXT):
+          t.is(file.fieldname, "alottext");
+          t.is(file.size, readFileSync(FIXTURE_PATHS.LONG_TEXT).byteLength);
+          break;
+        default:
+          t.fail("Unknown file found");
+      }
+    }
+    t.is(ctx.request.body.name, "sanic");
+  });
+  const { port } = await t.context.listen(app);
+  const form = new FormData();
+  form.append("animage", createReadStream(FIXTURE_PATHS.SMALL_JPEG));
+  form.append("sometext", createReadStream(FIXTURE_PATHS.SHORT_TEXT));
+  form.append("alottext", createReadStream(FIXTURE_PATHS.LONG_TEXT));
+  form.append("name", "sanic");
+  await fetch("http://localhost:" + port, {
+    method: "post",
+    headers: form.getHeaders(),
+    body: form
+  });
+});
+
+test("multipart > fields", async t => {
+  const app = new Koa();
+  t.plan(1);
+  app.use(multipart({ storageEngine: new MemoryStorage() })).use(ctx => {
+    ctx.body = "im done";
+    const bod = ctx.request.body;
+    t.deepEqual(bod, {
+      triforce: [
+        { wielder: "Link", name: "Courage" },
+        { wielder: "Zelda", name: "Wisdom" },
+        { wielder: "Ganondorf", name: "Power" }
+      ],
+      superMarioBrothers: ["1", "2", "3"]
+    });
+  });
+  const { port } = await t.context.listen(app);
+  const form = new FormData();
+
+  form.append("triforce[0][wielder]", "Link");
+  form.append("triforce[0][name]", "Courage");
+  form.append("triforce[1][wielder]", "Zelda");
+  form.append("triforce[1][name]", "Wisdom");
+  form.append("triforce[2][wielder]", "Ganondorf");
+  form.append("triforce[2][name]", "Power");
+  form.append("superMarioBrothers[0]", 1);
+  form.append("superMarioBrothers[1]", 2);
+  form.append("superMarioBrothers[2]", 3);
+  await fetch("http://localhost:" + port, {
+    method: "post",
+    headers: form.getHeaders(),
+    body: form
+  });
+});
